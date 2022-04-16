bc002e872b4da6386e53a98c9a4f69067c06782c
Olga Marinina <youarewonderful@yandex.ru>
2022-04-16 21:59:37 +0700

    Добавила двумерный массив задач

--- index.php
+++ index.php
@@ -2,6 +2,44 @@
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 $projects = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"];
+$tasks = [
+    [
+        'to_do' => 'Собеседование в IT компании',
+        'deadline' => '01.12.2019',
+        'category' => 'Работа',
+        'done' => false
+    ],
+    [
+        'to_do' => 'Выполнить тестовое задание',
+        'deadline' => '25.12.2019',
+        'category' => 'Работа',
+        'done' => false
+    ],
+    [
+        'to_do' => 'Сделать задание первого раздела',
+        'deadline' => '21.12.2019',
+        'category' => 'Учеба',
+        'done' => true
+    ],
+    [
+        'to_do' => 'Встреча с другом',
+        'deadline' => '22.12.2019',
+        'category' => 'Входящие',
+        'done' => false
+    ],
+    [
+        'to_do' => 'Купить корм для кота',
+        'deadline' => null,
+        'category' => 'Домашние дела',
+        'done' => false
+    ],
+    [
+        'to_do' => 'Заказать пиццу',
+        'deadline' => null,
+        'category' => 'Домашние дела',
+        'done' => false
+    ]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
