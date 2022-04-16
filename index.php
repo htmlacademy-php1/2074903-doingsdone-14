9bd27918e23e0de5ba3254a050a24dbf7057cafc
Olga Marinina <youarewonderful@yandex.ru>
2022-04-16 19:31:54 +0700

    Обновила логическое условие для checked

--- index.php
+++ index.php
@@ -73,9 +73,7 @@ $show_complete_tasks = rand(0, 1);
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
                         <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                        <?php if ($show_complete_tasks == 1): ?>
-                         checked
-                        <?php endif; ?> >
+                        <?php if ($show_complete_tasks): ?> checked <?php endif; ?> >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -95,7 +93,7 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
-                    <?php if ($show_complete_tasks == 1): ?>
+                    <?php if ($show_complete_tasks): ?>
                       <tr class="tasks__item task task--completed">
                           <td class="task__select">
                             <label class="checkbox task__checkbox">
