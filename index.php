6deb5867e188e419f3d1d8e236aebb27c38b3319
Olga Marinina <youarewonderful@yandex.ru>
2022-04-19 20:43:25 +0700

    Подключила функцию из документа, а не копированием

--- index.php
+++ index.php
@@ -44,8 +44,7 @@ $tasks = [
 * Counts the number of tasks in the project/category
 *
 * @param array $tasks Array with iterable tasks
-* @param $project Project/category under review
-* @param $task['category'] The key by which the tasks are checked
+* @param string $project Project/category under review
 *
 * @return int $count The calculated number of tasks in the selected project
 */
@@ -58,28 +57,7 @@ function count_tasks(array $tasks, $project) {
         return ($count);
 };
 
-/**
- * Подключает шаблон, передает туда данные и возвращает итоговый HTML контент
- * @param string $name Путь к файлу шаблона относительно папки templates
- * @param array $data Ассоциативный массив с данными для шаблона
- * @return string Итоговый HTML
- */
-function include_template($name, array $data = []) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-};
+require_once('helpers.php');
 
 $page_content = include_template('main.php', [
     'tasks' => $tasks,
