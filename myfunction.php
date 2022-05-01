1a3157a6eaba2f565877fb386316fdd09f1efed5
Olga Marinina <youarewonderful@yandex.ru>
2022-05-01 13:56:00 +0700

    Сделала функцию на показ ошибки 404, сделала подключение другого контента во время ошибки

--- myfunction.php
+++ myfunction.php
@@ -35,12 +35,36 @@ function count_tasks(array $tasks, $project) {
  * In a request to the database, it creates an array based on the response or returns an error
  *
  * @param object $result response from our database to our request
- * @return string about our error or our array
+ * @return functino to print or create array
  */
 function array_or_error(object $result) {
     if (!$result) {
         return print('Ошибка запроса');
     } else {
         return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+};
+
+/**
+ * Returns code 404 when $project_id doesn't have request parameter or tasks
+ *
+ * @param int $project_id - our request parametr
+ * @param array $projects - our array of projects to check exist project id
+ * @param array $tasks our array with tasks for $project_id
+ * @return string $page_content with code 404
+ */
+function code_404($project_id, array $projects, array $tasks) {
+    if (!empty($project_id)) {
+        $project_exist = 0;
+        foreach ($projects as $project) {
+            if ($project_id === $project['id']) {
+                $project_exist++;
+            }
+        }
+        if (!$project_exist OR empty($tasks)) {
+            http_response_code(404);
+            $page_content = print('Ошибка 404. Страница, которую Вы ищете, не может быть найдена');
+            return ($page_content);
+        }
     }
 }
