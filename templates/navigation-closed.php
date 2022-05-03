ad22d51023d14fab1c9143d7d911e424376375f0
Olga Marinina <youarewonderful@yandex.ru>
2022-05-03 20:04:30 +0700

    Подключила новые шаблоны, для незарегистрированного пользователя

---
+++ templates/navigation-closed.php
@@ -0,0 +1,8 @@
+<section class="content__side">
+    <p class="content__side-info">Если у вас уже есть аккаунт, авторизуйтесь на сайте</p>
+
+    <a class="button button--transparent content__side-button" href="sign.php">Войти</a>
+</section>
+
+<main class="content__main"><?= $content; ?></main>
+
