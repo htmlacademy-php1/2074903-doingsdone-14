434903df5e822f22d60423e20b1dde5a82877f33
Olga Marinina <youarewonderful@yandex.ru>
2022-05-13 22:55:16 +0700

    Привела переменные к единообразию

--- templates/main.php
+++ templates/main.php
@@ -9,13 +9,13 @@
 <div class="tasks-controls">
     <nav class="tasks-switch">
         <a href="/" class="tasks-switch__item
-        <?php if (!($today OR $tomorrow OR $overdue)): ?>tasks-switch__item--active<?php endif; ?>">Все задачи</a>
+        <?php if (empty($today OR $tomorrow OR $overdue)): ?>tasks-switch__item--active<?php endif; ?>">Все задачи</a>
         <a href="/?today=1" class="tasks-switch__item
-        <?php if ($today): ?>tasks-switch__item--active<?php endif; ?>">Повестка дня</a>
+        <?php if (!empty($today)): ?>tasks-switch__item--active<?php endif; ?>">Повестка дня</a>
         <a href="/?tomorrow=1" class="tasks-switch__item
-        <?php if ($tomorrow): ?>tasks-switch__item--active<?php endif; ?>">Завтра</a>
+        <?php if (!empty($tomorrow)): ?>tasks-switch__item--active<?php endif; ?>">Завтра</a>
         <a href="/?overdue=1" class="tasks-switch__item
-        <?php if ($overdue): ?>tasks-switch__item--active<?php endif; ?>">Просроченные</a>
+        <?php if (!empty($overdue)): ?>tasks-switch__item--active<?php endif; ?>">Просроченные</a>
     </nav>
 
     <label class="checkbox">
