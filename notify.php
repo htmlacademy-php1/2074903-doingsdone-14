4a93d3b9b8637e347a7c622a1666000e76618ac0
Olga Marinina <youarewonderful@yandex.ru>
2022-05-15 19:43:06 +0700

    Исправила ошибки в уведомлениях

--- notify.php
+++ notify.php
@@ -4,7 +4,7 @@ require_once 'init.php';
 require_once 'model.php';
 require_once 'helpers.php';
 require_once 'myfunction.php';
-require_once 'config/smtp_password.php';
+require_once 'config/smtp.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -16,11 +16,11 @@ $mail = new PHPMailer();
 $mail->isSMTP();
 $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-$mail->Host = 'smtp.yandex.ru';
-$mail->Port = 465;
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->Host = $smtp['host'];
+$mail->Port = $smtp['port'];
+$mail->SMTPSecure = $smtp['secure'];
 $mail->SMTPAuth = true;
-$mail->Username = $smtp['mail'];
+$mail->Username = $smtp['username'];
 $mail->Password = $smtp['password'];
 
 $mail->setFrom('keks@phpdemo.ru', 'doingsdone');
@@ -30,15 +30,16 @@ $date = date('Y-m-d', strtotime("now"));
 foreach ($users_notify as $user) {
     $user_id = $user['id'];
     $tasks_notify = get_tasks_notify($con, $user_id, $date);
-    $tasks = array_column($tasks_notify, 'name');
-    $mail->addAddress($user['email'], $user['name']);
-    $mail->Subject = 'Уведомление от сервиса «Дела в порядке»';
-    $mail->Body = 'Уважаемый, ' . $user['name'] . '. У вас запланирована задача ' . implode(",", $tasks) . ' на ' . $date;
-    $mail->AltBody = 'Уважаемый, ' . $user['name'] . '. У вас запланирована задача ' . implode(",", $tasks) . ' на ' . $date;
-}
-
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
+    if (!empty($tasks_notify)) {
+        $tasks = array_column($tasks_notify, 'name');
+        $mail->addAddress($user['email'], $user['name']);
+        $mail->Subject = 'Уведомление от сервиса «Дела в порядке»';
+        $mail->Body = 'Уважаемый, ' . $user['name'] . '. У вас запланирована задача ' . implode(", ", $tasks) . ' на ' . $date;
+        $mail->AltBody = 'Уважаемый, ' . $user['name'] . '. У вас запланирована задача ' . implode(", ", $tasks) . ' на ' . $date;
+        if (!$mail->send()) {
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message sent!';
+        }
+    }
 }
